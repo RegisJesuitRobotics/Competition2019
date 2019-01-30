4a2a33afad9a32078048d3fcb8e21e612edfcfaf
2018DriverStation <no@email.com>
2019-01-30 16:18:35 -0700

    backup

--- src/main/java/frc/robot/Lift.java
+++ src/main/java/frc/robot/Lift.java
@@ -1,11 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.Controller;;
 
 public class Lift {
@@ -24,15 +22,17 @@ public class Lift {
         SmartDashboard.putNumber("Lift Encoder", liftMotorEncoder.getPosition());
         SmartDashboard.putNumber("Lift Encoder 2", liftMotorEncoder.getPosition());
         if (_playstation.ButtonTriangle() == true) {
-            if(liftMotorEncoder.getPosition() == 72){
+            //if(liftMotorEncoder.getPosition() < 72){
             liftMotor.set(0.1);
             SmartDashboard.putNumber("Lift Encoder", liftMotorEncoder.getPosition());
             SmartDashboard.putBoolean("Going Up", _playstation.ButtonTriangle());
-            }
+            //}
         } else if (_playstation.ButtonX() == true) {
+            //if(liftMotorEncoder.getPosition() > 0){
             liftMotor.set(-0.1);
             SmartDashboard.putNumber("Lift Encoder 2", liftMotorEncoder.getPosition());
             SmartDashboard.putBoolean("Going Down", _playstation.ButtonX());
+            //}
         } else {
             liftMotor.set(0);
             SmartDashboard.putNumber("Lift Encoder 3", liftMotorEncoder.getPosition());
